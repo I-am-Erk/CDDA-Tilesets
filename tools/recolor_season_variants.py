7279d7357c4e676a3cd09ebb4a261ededf78cebf
Fris0uman <41293484+Fris0uman@users.noreply.github.com>
2021-05-23 23:32:35 +0200

    [HM] Add HollowMoon tileset from @secretstamos to the repo (#608)
    * several spear icons
    
    * Work done at my parents' house : )
    
    * January Update
    
    * Move content to proper folder
    
    * Transfert scrap folder missed by cherry picking
    
    * pngs_giant_terrain_64x64 folder
    
    * pngs_ammo/
    
    * pngs_huge_monsters_48x48/
    
    * pngs_large_monsters_32x32/
    
    * pngs_normal_characters_24x24/
    
    * pngs_normal_clothing_24x24/
    
    * pngs_normal_food_24x24/
    
    * pngs_normal_items_24x24/
    
    * pngs_normal_monsters_24x24/
    
    * pngs_normal_objects_24x24/
    
    * pngs_normal_weapons_24x24/
    
    * pngs_normal_weapons_melee_24x24/
    
    * tile_info.json and tileset.txt
    
    * Add script to create the four season variants in Hollow Moon tileset
    
    * Remove unnecessary generic file and folder and add a palette image to the template folder
    
    * remove battleaxe copies
    
    * Collapse all 24x24 folder into one
    
    * Reorganise other folders by size first and then by type of entity
    
    Co-authored-by: secretstamos <secretstamos@gmail.com>

---
+++ tools/recolor_season_variants.py
@@ -0,0 +1,77 @@
+import glob, os, shutil #pip install glob3
+import numpy as np #pip install numpy
+from PIL import Image
+import sys
+
+numberPrefixes = ["_0", "_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8"]
+seasonPrefixes = {
+  "_autumn": [(230,227,171), (216,147,110), (163,100,92), (90,68,72)],
+  "_winter": [(254,254,254), (203,200,218), (137,119,142), (90,68,72)],
+  "_summer": [(255,216,148), (194,176,107), (83,138,106), (73,75,88)]
+}
+defaultColors = [(240,236,187), (181,175,105), (115,130,92), (95,76,53)]
+
+def replaceColors(seasonFile, season):
+    replacementColors = seasonPrefixes[season]
+    img = Image.open(seasonFile).convert('RGB')
+    data = np.array(img)
+    currentColor = 0
+    
+    for color in defaultColors:
+        data[(data == color).all(axis = -1)] = replacementColors[currentColor]
+        currentColor = currentColor + 1
+
+    return Image.fromarray(data, mode='RGB')
+
+
+def transparent_background( img ):
+    img = img.convert("RGBA")
+    img_datas = img.getdata()
+    newData = []
+    for item in img_datas:
+        if item[0] == 21 and item[1] == 19 and item[2] == 21:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    return img
+
+def change_colors():
+    dirName = os.path.basename(os.getcwd())
+    for file in glob.glob("*.png"):
+        newFile = file
+        prefix = ""
+        if newFile.endswith(".png"):
+            newFile = newFile[:-4]
+            for number in numberPrefixes:
+                if newFile.endswith(number):
+                    newFile = newFile[:-2]
+                    prefix = number
+                    break
+
+            for season in seasonPrefixes:
+                seasonFile = f"{dirName}"
+                seasonFile = seasonFile + season
+                seasonFile = seasonFile + prefix
+                seasonFile = seasonFile + ".png"
+                shutil.copy2(file, seasonFile)
+                img = replaceColors(seasonFile, season)
+                transparent_background( img ).save(seasonFile, "PNG")
+
+        new_name = f"{dirName}"+"_spring.png"
+        os.rename(file,new_name)
+        transparent_background( Image.open(new_name) ).save(new_name, "PNG")
+        print(file)
+
+
+
+rootdir = sys.argv[1]
+
+
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file == "generic.png":
+            os.chdir(subdir)
+            change_colors()
