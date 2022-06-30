6f046562e12119ea0857b3c5aac867528c00fab1
vetal l <i.vetal@gmail.com>
2022-06-30 15:48:05 +0300

    Update tools/updtset.cmd
    Co-authored-by: Fris0uman <41293484+Fris0uman@users.noreply.github.com>

--- tools/updtset.cmd
+++ tools/updtset.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 setlocal EnableExtensions
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::ver 1.13 / 2022-06-29::::
-:: this script does following:
+:: this script does the following:
 :: - compose tileset from the source
 :: - put composed tileset into some folder for future use
 :: - copy said composed tileset from the folder into the game so you can try in on the fly
