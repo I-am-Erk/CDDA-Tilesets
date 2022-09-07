e3445ae516754c8ac8c2d9fa60a77137e709edb7
mlange-42 <martin_lange_@gmx.net>
2022-09-07 14:02:27 +0200

    add argument --path to unslice in another directory

--- tools/unslice_multitile.py
+++ tools/unslice_multitile.py
@@ -4,6 +4,7 @@ Combine a set of multitile sprites into 4x4 grid
 '''
 
 import argparse
+import os
 import pyvips
 
 from slice_multitile import MAPS
@@ -13,16 +14,20 @@ def main(args):
     multitile_dict = {}
 
     for suffix, position in MAPS[16].items():
-        sprite = pyvips.Image.new_from_file(f'{args.tile}_{suffix}.png')
+        sprite = pyvips.Image.new_from_file(os.path.join(args.path, f'{args.tile}_{suffix}.png'))
         multitile_dict[position] = sprite
 
     output_img = pyvips.Image.arrayjoin(
         [multitile_dict[i] for i in range(16)], across=4)
-    output_img.pngsave(f'{args.tile}_multitile.png')
+    output_img.pngsave(os.path.join(args.path, f'{args.tile}_multitile.png'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Combine a set of multitile sprites')
     parser.add_argument('tile', help='base name of the tile')
+    parser.add_argument('--path',
+                        required=False,
+                        default=".",
+                        help='path to the directory of the tile')
     main(parser.parse_args())
